7f6cce197ee8ef6526298668ac92236f20c20965
Vladlen <vladli2000610@gmail.com>
2022-01-03 16:57:21 +0600

    Responsibility division

--- main.py
+++ main.py
@@ -5,26 +5,28 @@ import win32api, win32con
 import pyautogui as pg
 
 from pyautogui import FailSafeException
+from settings import *
 
 
 #main
 is_turned = False
 is_in_progress = False
 is_clicking = False
-monitor_num = 2
-all_pos = {
-	1: (1051, 900),
-	2: (2653, 926)
-}
-all_det_pos = {
-	1: (345, 970),
-	2: (2240, 982),
-}
-red = (255, 0, 0)
-gray = (229, 229, 229)
-yellow = (255, 216, 0)
 pos = all_pos[monitor_num]
 det_pos = all_det_pos[monitor_num]
+
+def make_click():
+	if pg.pixelMatchesColor(pos[0], pos[1], red):
+		mouse_pos = pg.position()
+		try:
+			pg.click(mouse_pos[0], mouse_pos[1])
+		except KeyboardInterrupt:
+			sys.exit()
+		except FailSafeException:
+			pg.moveTo(500, 500)
+		return True
+	return False
+
 
 while True:
 	if keyboard.is_pressed('+'):
@@ -42,16 +44,8 @@ while True:
 			pg.press('4')
 			is_in_progress = True
 		if pg.pixelMatchesColor(det_pos[0], det_pos[1], yellow):
-			if pg.pixelMatchesColor(pos[0], pos[1], red):
-				mouse_pos = pg.position()
-				try:
-					pg.click(mouse_pos[0], mouse_pos[1])
-				except KeyboardInterrupt:
-					sys.exit()
-				except FailSafeException:
-					pg.moveTo(500, 500)
-				is_clicking = True
-			elif pg.pixelMatchesColor(pos[0], pos[1], gray):
+			is_clicking = make_click()
+			if pg.pixelMatchesColor(pos[0], pos[1], gray):
 				continue
 			else:
 				if is_in_progress and is_clicking:
